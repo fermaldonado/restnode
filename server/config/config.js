13c4999be93aba221829f48278e2099f7713e93b
fermaldonado <fmaldonado@tiempodevelopment.com>
2018-08-02 17:14:55 -0500

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,25 @@
 // ====================================================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ====================================================
+// Enviroment
+// ====================================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ====================================================
+// DB
+// ====================================================
+
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev' ){
+	urlDB = 'mongodb://localhost:27017/coffee';
+}else{
+	urlDB = 'mongodb://coffee-user:coffee123456@ds151153.mlab.com:51153/coffee';
+}
+
+process.env.URL_DB = urlDB;
---
+++ server/routes/usuario.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+const app = express();
+
+app.get('/usuario', function (req, res) {
+	let from = req.query.from || 0;
+	from = Number(from);
+
+	let limit = req.query.limit || 8;
+	limit = Number(limit);
+
+	Usuario.find({ estado: true }, 'nombre email role estado google img')
+		.skip(from)
+		.limit(limit)
+		.exec( (err, usuarios) => {
+			if(err){
+				return res.status(400).json({
+					ok:false,
+					err
+				});
+			}
+
+			Usuario.count({estado: true}, (err,conteo) => {
+				res.json({
+					ok: true,
+					usuarios,
+					cuantos: conteo
+				});
+			});
+
+			
+
+		});
+
+});
+
+app.post('/usuario', function (req, res) {
+	let body = req.body;
+
+	let usuario = new Usuario({
+		nombre: body.nombre,
+		email: body.email,
+		password: bcrypt.hashSync(body.password, 10),
+		role: body.role
+	});
+
+	usuario.save((err, userDB) => {
+		if(err){
+			return res.status(400).json({
+				ok:false,
+				err
+			});
+		}
+
+		res.json({
+			ok: true,
+			usuario: userDB
+		})
+	});
+
+	/*if(body.nombre === undefined){
+		res.status(400).json({
+			ok: false,
+			mensaje: 'Name is not optional'
+		});
+	}else{
+		res.json({
+			persona: body
+		});
+	}*/
+});
+
+app.put('/usuario/:id', function (req, res) {
+	let id = req.params.id;
+	let body = _.pick(req.body, ['nombre', 'img', 'email', 'role', 'estado']);
+
+	let options = {
+		new: true,
+		runValidators: true
+	};
+
+	Usuario.findByIdAndUpdate(id, body, options, (err, userDB) => {
+
+		if(err){
+			return res.status(400).json({
+				ok:false,
+				err
+			});
+		}
+
+		
+		if(!userDB){
+			return res.status(400).json({
+				ok:false,
+				err: {
+					message: 'User doesnt found'
+				}
+			});
+		}
+
+		res.json({
+		  	ok: true,
+		  	usuario: userDB
+		});
+
+	});
+});
+
+app.delete('/usuario/:id', function (req, res) {
+  	
+	let id = req.params.id;
+	let changeStatus = {
+		'estado' : false
+	};
+
+	Usuario.findByIdAndUpdate(id, changeStatus, {new:true}, (err, userDB) => {
+		if(err){
+			return res.status(400).json({
+				ok:false,
+				err
+			});
+		}
+
+		res.json({
+			ok: true,
+			usuario: userDB
+		});
+	});
+
+});
+
+module.exports = app;
