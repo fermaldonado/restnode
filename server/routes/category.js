b040e92ebea755e2c849050840bf0450a19cb0b1
fermaldonado <fmaldonado@tiempodevelopment.com>
2018-08-08 18:34:04 -0500

    Changes by lint

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,91 +1,85 @@
 const express = require('express');
 const Category = require('../models/category');
-const { verifyToken, verifyAdminRole } = require('../middlewares/auth');
+const {verifyToken, verifyAdminRole} = require('../middlewares/auth');
 const app = express();
 
 //=======================================
 // Get Category
 //=======================================
 app.get('/category', verifyToken, (req, res) => {
-	Category.find({})
-	.sort('description')
-	.populate('usuario', 'nombre email')
-	.exec((err, categories) => {
-		if (err) {
-			return res.status(500).json({
-				ok:false,
-				err
-			});
-		}
+    Category.find({})
+        .sort('description')
+        .populate('usuario', 'nombre email')
+        .exec((err, categories) => {
+        if (err) {
+            return res.status(500).json({ok: false, err});
+        }
 
-		res.json({
-			ok: true,
-			categories
-		})
-	});
+        res.json({ok: true, categories});
+        });
 });
 
 //=======================================
 // Get Category By Id
 //=======================================
 app.get('/category/:id', verifyToken, (req, res) => {
-	let id = req.params.id;
+    let id = req.params.id;
 
-	Category.findById(id, (err, categoryDB) => {
-		if (err) {
-			return res.status(500).json({
-				ok:false,
-				err
-			});
-		}
+    Category.findById(id, (err, categoryDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
 
-		if (!categoryDB) {
-			return res.status(400).json({
-				ok:false,
-				err: {
-					message: 'Id incorrect'
-				}
-			});
-		}
+        if (!categoryDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Id incorrect'
+                }
+            });
+        }
 
-		res.json({
-			ok: true,
-			category: categoryDB
-		});
-	});
+        res.json({
+            ok: true,
+            category: categoryDB
+        });
+    });
 });
 
 //=======================================
 // New Category
 //=======================================
 app.post('/category', verifyToken, (req, res) => {
-	let body = req.body;
+    let body = req.body;
 
-	let category = new Category({
-		description: body.description,
-		usuario: req.usuario._id
-	});
+    let category = new Category({
+        description: body.description,
+        usuario: req.usuario._id
+    });
 
-	category.save((err, categoryDB) => {
-		if (err) {
-			return res.status(500).json({
-				ok:false,
-				err
-			});
-		}
+    category.save((err, categoryDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
 
-		if (!categoryDB) {
-			return res.status(400).json({
-				ok:false,
-				err
-			});
-		}
+        if (!categoryDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
 
-		res.json({
-			ok: true,
-			category: categoryDB
-		});
-	});
+        res.json({
+            ok: true,
+            category: categoryDB
+        });
+    });
 });
 
 //=======================================
@@ -93,62 +87,62 @@ app.post('/category', verifyToken, (req, res) => {
 //=======================================
 app.put('/category/:id', verifyToken, (req, res) => {
 
-	let id = req.params.id;
-	let options = {
-		new: true,
-		runValidators: true
-	};
+    let id = req.params.id;
+    let options = {
+        new: true,
+        runValidators: true
+    };
 
-	Category.findByIdAndUpdate(id, { description: req.body.description }, options, (err, categoryDB) => {
-		if (err) {
-			return res.status(500).json({
-				ok:false,
-				err
-			});
-		}
+    Category.findByIdAndUpdate(id, {description: req.body.description}, options, (err, categoryDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
 
-		if (!categoryDB) {
-			return res.status(400).json({
-				ok:false,
-				err
-			});
-		}
+        if (!categoryDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
 
-		res.json({
-			ok: true,
-			category: categoryDB
-		});
-	});
+        res.json({
+            ok: true,
+            category: categoryDB
+        });
+    });
 });
 
 //=======================================
 // Delete Category
 //=======================================
 app.delete('/category/:id', [verifyToken, verifyAdminRole], (req, res) => {
-	let id = req.params.id;
+    let id = req.params.id;
 
-	Category.findByIdAndRemove(id, (err, categoryDB) => {
-		if (err) {
-			return res.status(500).json({
-				ok:false,
-				err
-			});
-		}
+    Category.findByIdAndRemove(id, (err, categoryDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
 
-		if (!categoryDB) {
-			return res.status(400).json({
-				ok:false,
-				err: {
-					message: 'Id does not exist'
-				}
-			});
-		}
+        if (!categoryDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Id does not exist'
+                }
+            });
+        }
 
-		res.json({
-			ok: true,
-			message: 'Category deleted'
-		});
-	});
+        res.json({
+            ok: true,
+            message: 'Category deleted'
+        });
+    });
 });
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('./config/config.js')
+require('./config/config.js');
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -8,22 +8,22 @@ const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
- 
+
 // parse application/json
 app.use(bodyParser.json());
 
 // Make public accesible
 app.use(express.static(path.resolve(__dirname , '../public' )));
 
-// Routes configuration 
+// Routes configuration
 app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URL_DB, {useNewUrlParser: true}, (err, res) => {
-	if (err) throw err;
+    if (err) throw err;
 
-	console.log('Database online');
+    console.log('Database online');
 });
 
 app.listen(process.env.PORT, () => {
-	console.log("Listening port ",process.env.PORT);
+    console.log("Listening port ",process.env.PORT);
 });
